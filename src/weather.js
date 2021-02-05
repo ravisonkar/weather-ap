5a090d6a77c5d9c151b556142dff61055e599b21
ravi <sonkarravi44@gmail.com>
2021-02-05 13:58:46 +0530

    fix

--- src/weather.js
+++ src/weather.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const fetchWeatherDataRequest = (selectValue) => { 
     return axios
-    .get(`https://weather-app-vue-vuex.netlify.com/api.openweathermap.org/data/2.5/weather?zip=${selectValue}&appid=32feb6bc3cb00957d53449796352cde7`)
+    .get(`http://weather-app-vue-vuex.netlify.com/api.openweathermap.org/data/2.5/weather?zip=${selectValue}&appid=32feb6bc3cb00957d53449796352cde7`)
       .then((value) => value.data);
 };
